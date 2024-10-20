6977b3daa89ba60687908d9e59a58c206c575b52
Chrissie22 <christabel.ojobolo@gmail.com>
2024-10-20 16:45:16 +0100

    Update on the settings file

--- EcommerceAPI/settings.py
+++ EcommerceAPI/settings.py
@@ -154,6 +154,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-# Media files (Uploaded by users)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
