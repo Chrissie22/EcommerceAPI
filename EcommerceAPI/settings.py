547e63d81abdc64868a667bfc54286471150dfaf
Chrissie22 <christabel.ojobolo@gmail.com>
2024-10-20 15:36:22 +0100

    database

--- EcommerceAPI/settings.py
+++ EcommerceAPI/settings.py
@@ -101,11 +101,14 @@ WSGI_APPLICATION = 'EcommerceAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'IUBfoTpovsUvJZpqZGuPlccFZsxteezW',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '54220'
     }
 }
-
 
 #model settings
 
