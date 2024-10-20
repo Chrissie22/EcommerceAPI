030195ac46f3d330e18eadb340c69c786aed0109
Chrissie22 <christabel.ojobolo@gmail.com>
2024-10-20 17:57:01 +0100

    swagger ui

--- EcommerceAPI/urls.py
+++ EcommerceAPI/urls.py
@@ -20,6 +20,22 @@ from DjangoAPIapp.views import RegistrationAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API Title",
+      default_version='v1',
+      description="API documentation",
+      terms_of_service="https://www.yourcompany.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourcompany.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +44,9 @@ urlpatterns = [
     path('auth/register/', RegistrationAPIView.as_view(), name='register'),
     path('auth/login/', TokenObtainPairView.as_view(), name='login'),
     path('auth/refresh-token', TokenRefreshView.as_view(), name='refreshtoken'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('openapi/', schema_view.without_ui(cache_timeout=0), name='schema-openapi-json'),  # OpenAPI spec in JSON format
+
 
 ]
